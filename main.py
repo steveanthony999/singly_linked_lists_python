cc7cf50068f58000a20de35ce17631347b201d2b
Steven Woodward <steveanthony999@gmail.com>
2023-09-06 05:36:47 -0700

    Added Node class and commentary

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello World")
+# A Node represents a single element in a singly linked list.
+# It contains data (the value of the element) and a reference to the next node in the sequence.
+# If the next reference is None, it indicates the end of the list.
+
+
+# Define the Node class to represent individual elements in a linked list.
+class Node:
+    # Constructor for the Node class.
+    # Initializes a node with a given value and an optional reference to the next node.
+    # If no next node is provided, it defaults to None, indicating the end of the list.
+    def __init__(self, value, next_node=None):
+        self.value = value  # The data value stored in the node.
+        self.next_node = next_node  # Reference to the next node in the linked list.
